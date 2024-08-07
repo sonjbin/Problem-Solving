cb788c191c80d85f981ad2030337ad7d92468265
sonjbin <52647915+sonjbin@users.noreply.github.com>
2024-08-07 12:49:52 +0900

    Time: 152 ms (79.87%), Space: 27.9 MB (55.92%) - LeetHub

--- LeetCode/0189-rotate-array/0189-rotate-array-slicing.py
+++ LeetCode/0189-rotate-array/0189-rotate-array-slicing.py
@@ -3,15 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if k >= len(nums):
-            k = k%len(nums)
-        def reverse_inplace(nums, n, m):
-            for i in range((m-n+1)//2):
-                temp = nums[m-i]
-                nums[m-i] = nums[n+i]
-                nums[n+i] = temp
-
-        reverse_inplace(nums, 0, len(nums)-k-1)
-        reverse_inplace(nums, len(nums)-k, len(nums)-1)
-        nums.reverse()
+        k = k%len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
 
